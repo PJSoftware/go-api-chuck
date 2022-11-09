292ac4a0eb82831b5b25bf504986a29ba8ea41f9
Peter Jones <pjsoftware@petesplace.id.au>
2022-11-09 13:09:06 +1000

    add cmd/chuck to permit package testing

---
+++ cmd/chuck/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	chuck "github.com/pjsoftware/go-api-chuck"
+)
+
+type Joke struct {
+	Value string
+}
+
+func main() {
+	cn := chuck.New()
+	fmt.Println("Chuck [Auth0: No Authorisation]")
+	cj := cn.RandomJoke()
+	fmt.Printf("\n%s\n\n", cj.Value)
+	fmt.Printf("- ID: \"%s\"\n", cj.ID)
+	if len(cj.Categories) > 0 {
+		fmt.Printf("- Categories: %v\n", cj.Categories)
+	}
+}
